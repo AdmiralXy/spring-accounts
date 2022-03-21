fd16139504d8000df73096ad24bc089f541e53ce
AdmiralXy <gameproblem80@gmail.com>
2022-03-21 17:16:17 +0300

    add forms data auto trim

---
+++ src/main/java/com/admiralxy/springaccounts/controller/ControllerSetup.java
@@ -0,0 +1,17 @@
+package com.admiralxy.springaccounts.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class ControllerSetup
+{
+    @InitBinder
+    public void initBinder(WebDataBinder binder)
+    {
+        StringTrimmerEditor stringTrimmer = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimmer);
+    }
+}
