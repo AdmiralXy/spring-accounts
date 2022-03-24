447b8f5d78d5b6ee0ecb93ca485912762d160ff9
AdmiralXy <gameproblem80@gmail.com>
2022-03-24 22:34:44 +0300

    add credential service test boilerplate

---
+++ src/test/java/com/admiralxy/springaccounts/service/CredentialServiceTest.java
@@ -0,0 +1,33 @@
+package com.admiralxy.springaccounts.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CredentialServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getAllByUser() {
+    }
+}
